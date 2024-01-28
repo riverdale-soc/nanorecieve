e9af103eab9c0c951235bc8ba505a7f4a9104287
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2024-01-28 14:00:44 -0500

    Create DCC_Recieve.py
    Init template for dcc recevie over serial USB

---
+++ DCC_Recieve.py
@@ -0,0 +1,65 @@
+"""
+Author: Sophia Ventresca
+ESP32 Submersion Handler UART Listener
+Opens USB-Serial Port and waits for Wake String, then constructs MOB Wake Dict
+"""
+
+import serial
+import time
+import enum
+
+# Enum of valid MOB States
+class MOB_STATE(enum.Enum):
+    MOB_WAKE = "OK"
+    MOB_RESET = "RESET"
+    MOB_NONE = "NONE"
+
+class DCCListener:
+    def __init__(self):
+        """
+          Open Serial Port to ESP32. 
+          UART Configs: 115200 Baud, 8N1, XON/XOFF, PARITY NONE, RTS/CTS, 
+        """
+        self.port = serial.Serial('/dev/ttyUSB0', baudrate=115200, xonxoff=True)
+        if self.port.is_open is not True:
+            self.port.open()
+
+        self.mob_wake_dict = {
+            MOB_STATE : MOB_NONE, 
+            Altitude : "",
+            Longitude : "",
+            Latitude : "",
+        }
+
+        self.mob_time = None
+
+    # Read all lines from serial port, and parse for MOB Wake String
+    def listen(self):
+        while True:
+            line = self.port.readline().decode('utf-8')
+            if self.parse_line(line) is True:
+                # Implement wake up logic here
+                print("MOB Wake String Received")
+            print(line)
+
+    # Parse line for MOB Wake String
+    # We will assume it will come in a single line like: 
+    # MOB indicates MOB Wake String, MOB State, Altitude, Longitude, Latitude
+    #        "MOB: OK, 12345.12345, 12345.12345, 12345.12345"
+    def parse_line(self, line: str) -> bool:
+        # Read line from serial port
+        # Parse line for MOB Wake String, if true, update MOB Wake Dict
+        # if it is not a MOB Wake String, return false
+
+
+        # Store timestamp of MOB Wake String found 
+        self.mob_time = time.time()
+        return False
+ 
+    def close_port(self) -> bool:
+        self.port.close()
+        return 0
+
+if __name__ == '__main__':
+    dcclisten = DCCListener()
+    dcclisten.listen()
